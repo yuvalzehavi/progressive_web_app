20da07fc27cd0fe3a5c92c5f34891a8463337ab7
Yuval Zehavi <yzehavi@playags.com>
2018-11-21 19:38:54 +0200

    first setup

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode:'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+		new HtmlWebpackPlugin()
+	]
+};
